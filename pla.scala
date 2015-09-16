9fbf19f7188e5468756cd03204483b9785e165ca
Krishnan Raman <kr61107@gmail.com>
2015-09-16 14:53:27 -0700

    Update pla.scala
    Your code doesn't quite work ( both the original & my rewrite ). Would appreciate if you can bugfix. Thanks!

--- pla.scala
+++ pla.scala
@@ -1,41 +1,24 @@
-import scala.util.Random.nextDouble
-
 object pla extends App {
     def looper(count: Int,loop: Int,streak: Int,w: Seq[Double],vectors: Seq[Seq[Double]],signs: Seq[Int]): Seq[Double] = {
-        if (count == 100000)
-            w
-        else if (streak == 100){
-            w
-        }
-        else {
-            val loop_check = if(loop > 99) 0 else loop
-            val cur_vec = vectors(loop_check)
-            val cur_sign = signs(loop_check)
-            val check = sign_check(cur_vec,w)
-            if (check == cur_sign){              
-                looper(count+1,loop_check+1,streak+1,w,vectors,signs)
-            }
-            else if (check == -1){
-                val new_vector = plus(w,(cur_vec ++ Seq(1.0)).map{i =>.01 * i}.toSeq)
-                looper(count+1,loop_check+1,0,new_vector,vectors,signs)
-            }
-            else{
-                val new_vector_1 = plus(w,(cur_vec ++ Seq(1.0)).map{j =>.01 * j}.toSeq)
-                looper(count+1,loop_check+1,0,new_vector_1,vectors,signs)
-            }
+        (count == 100000) || (streak == 100) match {
+            case true => w // terminate recursion
+            case _ => // recurse
+                val i= if(loop > 99) 0 else loop
+                val cur_vec = vectors(i)
+                val cur_sign = signs(i)
+                val check = sign_check(cur_vec,w)
+                val W = plus(w,(cur_vec ++ Seq(1.0)).map{j =>.01 * j}.toSeq)
+                looper(count+1,i+1,if (check==cur_sign) streak + 1 else 0, if (check==cur_sign) w else W,vectors,signs)
         }
     }
-            
-            
 
-    def dot(a:Seq[Double],b:Seq[Double]) = a.zip(b).map { case (c,d) => c * d } sum
+    def dot(a:Seq[Double],b:Seq[Double]) = a.zip(b).map { case (c,d) => c * d }.sum
     def plus(a:Seq[Double],b:Seq[Double]) = a.zip(b).map { case (c,d) => c + d }
-    def sign(x: Double) = {if (x == 0) 0 else if (x > 0) 1 else -1}
-    def sign_check(x: Seq[Double], y: Seq[Double]) = {sign(dot(y,x ++ Seq(1.0)))}
+    def sign_check(x: Seq[Double], y: Seq[Double]) = math.signum(dot(y,x ++ Seq(1.0))).toInt
     
     val coef: Seq[Double] = Seq(2,3,4,-7)
-    val vectors = (0 until 100).map{i => Seq.fill(3)(-9.4 + 20*nextDouble())}.toSeq
-    val signs = vectors.map{v => sign_check(v,coef)}.toSeq
-    val random_weights = Seq.fill(4)(-9 + 20*nextDouble())
-    println(looper(0,0,0,random_weights,vectors,signs))
+    val vectors = (0 until 100).map{i => Seq.fill(3)(-9.4 + 20* util.Random.nextDouble())}.toSeq
+    val init = vectors.map{v => sign_check(v,coef)}.toSeq
+    val random_weights = Seq.fill(4)(-9 + 20* util.Random.nextDouble())
+    println(looper(0,0,0,random_weights,vectors,init))
 }
